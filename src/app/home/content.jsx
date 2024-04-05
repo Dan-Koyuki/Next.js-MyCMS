45a0d0dab683292b5a02a9aa18e5395e51643130
Dan-Koyuki <dankoyuki398@gmail.com>
2024-04-05 18:34:46 +0700

    add

--- src/app/home/content.jsx
+++ src/app/home/content.jsx
@@ -1,9 +1,131 @@
 import React from 'react'
 
-const Content = () => {
+const HomeContent = () => {
   return (
-    <div>Content</div>
-  )
+    <div className='overflow-auto'>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
+        assumenda accusamus illo quae sapiente at, architecto aliquid ullam
+        officiis laborum error!
+      </p>
+    </div>
+  );
 }
 
-export default Content
+export default HomeContent
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,141 +2,24 @@ import LandingTop from '@/components/nav/landtop.nav';
 import SideBar from '@/components/nav/sidebar.nav';
 import ProtectedRoute from '@/components/utils/protectedRoute.util'
 import React from 'react'
+import HomeContent from './content';
 
 const Home = () => {
   return (
     // <ProtectedRoute>
     <div
-      className="bg-[#EDF1D6] text-black h-screen flex flex-col overflow-hidden
+      className="absolute bg-[#EDF1D6] text-black flex flex-col overflow-hidden
         dark:bg-[#35374B] dark:text-white"
     >
-      <LandingTop />
+      <div className="sticky">
+        <LandingTop />
+      </div>
       <div className="flex flex-row w-full">
-        <div className="sticky w-1/4">
+        <div className="sticky w-1/4 overflow-hidden">
           <SideBar />
         </div>
         <div className="relative w-3/4">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            doloribus, perspiciatis impedit iure magnam, temporibus sunt ea aut
-            assumenda accusamus illo quae sapiente at, architecto aliquid ullam
-            officiis laborum error!
-          </p>
+          <HomeContent />
         </div>
       </div>
     </div>
